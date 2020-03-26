f2d65d5fd210709fef56cd091e715aaf63baa67d
vmykhali666 <vmykhali666@gmail.com>
2020-03-27 00:35:16 +0200

    файлы в паблике

--- twitter/gulpfile.js
+++ twitter/gulpfile.js
@@ -153,7 +153,7 @@ gulp.task("img", () => {
 
 gulp.task("fileInclude", callback => {
   gulp
-    .src([SRC + "/html/[^_]*.html"])
+    .src([SRC + "/html/home-page.html"])
     .pipe(
       fileInclude({
         prefix: "@@",
